524aa456f64879fe4cdf7e1adc41ce2e2646f73a
Luke Smith <stuff@lukesmith.net>
2016-11-04 19:51:52 +0000

    bump version

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "Cimple GitHub relay"
 	app.Usage = "Relay webhook requests from GitHub to a Cimple server"
-	app.Version = "0.0.2"
+	app.Version = "0.0.3"
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
 			Name:        "port",
